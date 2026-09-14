1682e62b4b7bbb8c0298a064485e06729057fcf9
Rudra Chavda <rudrachavda@Rudras-MacBook-Air.local>
2026-09-14 13:03:52 +0530

    working

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
 import Navbar from "./components/Navbar";
-
 import Hero from "./components/Hero";
-
 import About from "./components/About";
-
 import Skills from "./components/Skills";
-
 import Projects from "./components/Projects";
-
 import Contact from "./components/Contact";
-
 import Footer from "./components/Footer";
 
 function App() {
 
   return (
-
     <>
 
       <Navbar />
